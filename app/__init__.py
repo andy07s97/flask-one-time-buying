2e05142e295d4ab7cda68704c08e4cc19335bd04
andy07s97 <135204751+andy07s97@users.noreply.github.com>
2026-01-08 12:54:25 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,25 @@
 from flask import Flask
 from .config import load_config
 from .extensions import db, migrate
+
 from .routes.main import main_bp
 from .routes.auth import auth_bp
+from .routes.api import api_bp
+from .routes.ecpay import ecpay_bp
+
 
 def create_app():
     app = Flask(__name__)
     load_config(app)
 
+    # 初始化 extensions（單一 db 實例）
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Blueprints
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(api_bp)
+    app.register_blueprint(ecpay_bp)
 
     return app
